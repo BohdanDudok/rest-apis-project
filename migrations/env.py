e547f8e40507180fdba19324d97dc81c159a7e4c
Bohdan Dudok <bodiadudok19@gmail.com>
2023-02-03 20:42:11 +0200

    small change in dockerfile

--- migrations/env.py
+++ migrations/env.py
@@ -92,8 +92,8 @@ def run_migrations_online():
         context.configure(
             connection=connection,
             target_metadata=get_metadata(),
-            process_revision_directives=process_revision_directives,
             compare_types=True,
+            process_revision_directives=process_revision_directives, 
             **current_app.extensions['migrate'].configure_args
         )
 
